6bdb68645617b6392e2377c77faa765c1de561f1
erhilds <erickh.andreas@gmail.com>
2020-04-28 06:26:48 +0700

    feat: add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <app-navbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import AppNavbar from "@/components/AppNavbar";
+
+export default {
+  components: { AppNavbar }
+};
+</script>
 
 <style lang="scss">
 #app {
